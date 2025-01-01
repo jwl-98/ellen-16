7b0aabf2932e9f96e641f46cd6d849e87de3dc04
jwl-98 <apple_komputer@icloud.com>
2025-01-01 16:00:08 +0900

    initial commit

---
+++ MusicSearchApp/MusicSearchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicSearchApp
+//
+//  Created by 진욱의 Macintosh on 1/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
